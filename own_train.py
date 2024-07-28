29f356f2b241731bdb63b82528232987ab977088
AlekhinAVe <117147398+AlekhinAVe@users.noreply.github.com>
2024-07-28 15:58:23 +0300

    Update own_train.py

--- own_train.py
+++ own_train.py
@@ -1,5 +1,5 @@
 import model_loader
-import train_from_scratch
+import solving_train
 from PIL import Image
 from pathlib import Path
 from transformers import CLIPTokenizer
@@ -18,8 +18,17 @@ elif (torch.has_mps or torch.backends.mps.is_available()) and ALLOW_MPS:
 print(f"Using device: {DEVICE}")
 
 
-#tokenizer = CLIPTokenizer("../data/vocab.json", merges_file="../data/merges.txt")
+model_file = "../data/v1-5-pruned-emaonly.ckpt"
+models = model_loader.preload_models_from_standard_weights(model_file, DEVICE)
+#models = torch.load(model_file, DEVICE)
 
+#model_file_1 = "../data/model_checkpoint.ckpt"
+#checkpoint = torch.load(model_file_1, DEVICE)
+
+#models['diffusion'] = checkpoint['model_state_dict']
+#del models['diffusion']
+
+#torch.save(models, '../data/full_model.ckpt')
 
 #PROMPT
 
@@ -27,7 +36,7 @@ prompt = "increased image resolution"
 
 #IMAGE TO IMAGE
 
-strength = 1
+strength = 0.1
 
 #SAMPLER
 
@@ -36,29 +45,27 @@ num_inference_steps = 50
 seed = 42
 
 #CREATE THE DATA
-path = '/content/own__stable_diffusion/celebahq-resized-256x256/celeba_hq_256/'
+path = 'C:/Users/Andrey/faces/faces'
 data = DATA(path)
-print('done')
 data.create_data()
-print('done')
 data.create_dataloader()
-print('done')
 
 #SET PARAMETERS OF TRAINING
 
-batch_size = 64
-epochs = 1000
+batch_size = 1
+epochs = 20
 
-train_from_scratch.train(
+solving_train.train(
         prompt,
         data=data.data,
-        strength=1.0,
-        models=None,
+        strength=0.1,
+        models=models,
         seed=seed,
         device=DEVICE,
         idle_device="cpu",
         tokenizer=None,
-        batch_size=64,
-        epochs=1000,
-        num_training_steps=1000
+        batch_size=batch_size,
+        epochs=10,
+        num_training_steps=200,
+        checkpoint=False
 )
