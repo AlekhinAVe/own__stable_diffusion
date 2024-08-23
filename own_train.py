9eae72ea68d032b1b124fba5fb02ae853a10c851
AlekhinAVe <117147398+AlekhinAVe@users.noreply.github.com>
2024-08-23 05:02:58 +0300

    Update own_train.py

--- own_train.py
+++ own_train.py
@@ -1,10 +1,12 @@
 import model_loader
-import train_from_scratch
+import solving_train
+import sr_training
 from PIL import Image
 from pathlib import Path
 from transformers import CLIPTokenizer
 import torch
 from data import DATA
+import train_decoder
 
 DEVICE = "cpu"
 
@@ -18,7 +20,7 @@ elif (torch.has_mps or torch.backends.mps.is_available()) and ALLOW_MPS:
 print(f"Using device: {DEVICE}")
 
 
-model_file = "/content/drive/MyDrive/models/v1-5-pruned-emaonly.ckpt"
+model_file = "../data/v1-5-pruned-emaonly.ckpt"
 models = model_loader.preload_models_from_standard_weights(model_file, DEVICE)
 #models = torch.load(model_file, DEVICE)
 
@@ -36,37 +38,36 @@ prompt = "increased image resolution"
 
 #IMAGE TO IMAGE
 
-strength = 0.1
+strength = 1
 
 #SAMPLER
 
 sampler = "ddpm"
-num_inference_steps = 50
+num_inference_steps = 1000
 seed = 42
 
 #CREATE THE DATA
-path = '/content/celebahq-resized-256x256/celeba_hq_256'
+path = 'C:/Users/Andrey/faces/faces'
 data = DATA(path)
 data.create_data()
 data.create_dataloader()
 
 #SET PARAMETERS OF TRAINING
 
-batch_size = 1
-epochs = 20
+batch_size = 8
+epochs = 5
 
-train_from_scratch.train(
+solving_train.train(
         prompt,
         data=data.data,
-        strength=0.1,
+        strength=1,
         models=models,
         seed=seed,
         device=DEVICE,
         idle_device="cpu",
         tokenizer=None,
         batch_size=batch_size,
-        epochs=10,
-        num_training_steps=200,
+        epochs=5,
+        num_training_steps=1000,
         checkpoint=False
 )
-
