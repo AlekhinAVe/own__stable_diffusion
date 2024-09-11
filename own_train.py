cb275cae43263844bbd11787012b04259e63adfd
AlekhinAVe <117147398+AlekhinAVe@users.noreply.github.com>
2024-09-11 18:20:08 +0300

    Update own_train.py

--- own_train.py
+++ own_train.py
@@ -1,10 +1,12 @@
 import model_loader
 import solving_train
+import sr_training
 from PIL import Image
 from pathlib import Path
 from transformers import CLIPTokenizer
 import torch
 from data import DATA
+import train_decoder
 
 DEVICE = "cpu"
 
@@ -18,17 +20,20 @@ elif (torch.has_mps or torch.backends.mps.is_available()) and ALLOW_MPS:
 print(f"Using device: {DEVICE}")
 
 
-model_file = "/content/drive/MyDrive/models/v1-5-pruned-emaonly.ckpt"
+model_file = "../data/v1-5-pruned-emaonly.ckpt"
 models = model_loader.preload_models_from_standard_weights(model_file, DEVICE)
-#models = torch.load(model_file, DEVICE)
+models = torch.load(model_file, DEVICE)
 
-#model_file_1 = "../data/model_checkpoint.ckpt"
-#checkpoint = torch.load(model_file_1, DEVICE)
+model_file_1 = "../data/model_checkpoint_1.ckpt"
+checkpoint_1 = torch.load(model_file_1, DEVICE)
+
+model_file_2 = "../data/model_checkpoint_2.ckpt"
+checkpoint_2 = torch.load(model_file_2, DEVICE)
 
 #models['diffusion'] = checkpoint['model_state_dict']
 #del models['diffusion']
 
-#torch.save(models, '../data/full_model.ckpt')
+torch.save(models, '../data/full_model.ckpt')
 
 #PROMPT
 
@@ -45,14 +50,14 @@ num_inference_steps = 1000
 seed = 42
 
 #CREATE THE DATA
-path = '/content/celebahq-resized-256x256'
+path = 'C:/Users/Andrey/faces/faces'
 data = DATA(path)
 data.create_data()
 data.create_dataloader()
 
 #SET PARAMETERS OF TRAINING
 
-batch_size = 8
+batch_size = 1
 epochs = 5
 
 solving_train.train(
@@ -65,8 +70,10 @@ solving_train.train(
         idle_device="cpu",
         tokenizer=None,
         batch_size=batch_size,
-        epochs=5,
+        epochs=epochs,
         num_training_steps=1000,
-        checkpoint=False
+        checkpoint_1=False,
+        checkpoint_2=False
 )
+
 
